f133c7f2ae9fbbc5a1a10208aceb30829f06b929
castr <castr@FidelCastro>
2022-10-07 17:34:26 +0530

    FirstCommit

---
+++ LearningMaven/src/main/java/LearnMaven/OpenGoogle.java
@@ -0,0 +1,25 @@
+package LearnMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenGoogle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Castro\\Learning\\Driver\\chromedriver_win32\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("fidel");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		driver.close();
+
+	}
+
+}
